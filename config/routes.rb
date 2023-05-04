604ddbe1181d2645c5ef9e223a746191e088bd34
Anthony Marion <anthonycmarion@gmail.com>
2023-05-04 16:20:14 +1000

    Routes updated with resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'todos#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :todos
   # Defines the root path route ("/")
   # root "articles#index"
 end
